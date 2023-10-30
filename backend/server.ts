99524e2a5e794e5e8f2618da0190c157ffa41aa1
¨zKriguer¨ <kriguerfreitas2020@gmail.com>
2023-10-30 16:22:58 -0300

    fix: dotenv import
    insert dot env for server.ts

--- backend/server.ts
+++ backend/server.ts
@@ -2,6 +2,8 @@ import fastify from "fastify";
 import { productsController } from "./src/routes/products/products-controller";
 import cors from "@fastify/cors";
 import { categoryController } from "./src/routes/category/category-controller";
+import dotenv from "dotenv";
+dotenv.config();
 
 const app = fastify();
 
